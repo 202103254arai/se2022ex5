c77d919f1f09d91244002f8405ba99b18379651c
202103254arai <123754214+202103254arai@users.noreply.github.com>
2023-01-28 00:38:57 +0900

    Add files via upload

---
+++ se2022ex5/ex5-2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void) {
+  char a[] = "hello world";
+  printf("%s\n", a);
+  return 0;
+}
---
+++ se2022ex5/ex5-3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  char a[] = "helloworld";
+  printf("helloworld : %d\n", strlen(a));
+  return 0;
+}
